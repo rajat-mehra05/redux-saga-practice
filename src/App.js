6bf132475ea09c2c4c304550e06e2bea2e68394b
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-09-29 15:06:09 +0530

    feat: redux-saga-practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { getUsersFetch } from "./actions";
+import "./App.css";
 
 function App() {
+  const disptach = useDispatch();
+  const users = useSelector((state) => state.myReducer.users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="user-container">
+        <h1> Users </h1>
+        <button onClick={() => disptach(getUsersFetch())}> Get Users </button>
+
+        {users.map((user) => (
+          <p> {user.name} </p>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/sagas.js
@@ -0,0 +1,18 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+import { GET_USERS_FETCH, GET_USERS_SUCCESS } from "./actions";
+
+function usersFetch() {
+  return fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+    res.json()
+  );
+}
+
+//generator func
+export function* workGetUsersFetch() {
+  const users = yield call(usersFetch);
+  yield put({ type: GET_USERS_SUCCESS, users });
+}
+
+export default function* mySaga() {
+  yield takeEvery(GET_USERS_FETCH, workGetUsersFetch);
+}
